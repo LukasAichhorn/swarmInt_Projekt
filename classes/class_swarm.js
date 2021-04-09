49e8b59c808f5871bfce2ae30cb047138bd1ee0e
jacob-sch <jacob.schmauss@gmail.com>
2021-04-09 21:44:10 +0200

    swarm class

--- classes/class_swarm.js
+++ classes/class_swarm.js
@@ -1,8 +1,13 @@
 class Swarm {
-    constructor(numBots, bots) {
+    constructor(numBots, bots, endConditions) {
         this.numBots = numBots;
         this.bots = bots;
         this.taskCompleted = false;
+        //irgendein array aber noch nicht sicher was da drin sein soll
+        //evtl eine neue klasse?
+        //muss irgendwie überprüfbar sein
+        this.endConditions = endConditions;
+
 
     }
     addBot(bot) {
@@ -10,6 +15,33 @@ class Swarm {
         this.numBots += 1;
     }
 
-    
+    //early idea on how a swarm can know if all tasks are completed
+    //obv not finished!
+    checkTaskCompletion() {
+
+        this.endConditions.forEach(element => {
+            if(element == "completed Task") {
+
+            }
+            else{
+                return false;
+            }
+        });
+        return true;
+
+    }
+    updateStatus() {
+        if(this.checkTaskCompletion) {
+            this.taskCompleted = true;
+        }
+        else{
+            this.taskCompleted = false;
+        }
+
+
+    }
+
+
+
 
 }
