9a55763a8d07b9062f2ffa4ee6b3e2fa39ec2627
smindl <yasmineludwig@gmail.com>
2021-06-21 17:45:58 +0200

    manueller merge

--- UI/ui-generator.js
+++ UI/ui-generator.js
@@ -30,7 +30,6 @@ export function renderMenue(FORM, ToogleID, data) {
     renderAbilitySection(FORM, abilityOptions);
   } else {
     FORM.empty();
-
     addSelectElem(FORM, data, "Startbedingungen");
   }
 }
@@ -188,6 +187,12 @@ export function renderAbilitySection(target, optionList) {
   $(".dropdown-menu.keep-open").on("click", function (e) {
     e.stopPropagation();
   });
+
+  window.onclick = function(event) {
+    if (!event.target.matches(".dropdown-toggle")){
+      $(".dropdown-toggle").next().toggle();
+    }
+  };
 }
 export function createSetupArray() {}
 
